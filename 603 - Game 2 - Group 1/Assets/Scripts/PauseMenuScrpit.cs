e3bb713e6c8028e36efdbab0bcb8cee5ae51910b
parthrustagi <pr9269@g.rit.edu>
2022-02-02 12:57:23 -0500

    Added PausePanel

---
+++ 603 - Game 2 - Group 1/Assets/Scripts/PauseMenuScrpit.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScrpit : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    public GameObject pausePanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(isPaused)
+            {
+                gameResume();
+                Cursor.visible = false;
+            }
+
+            else
+            {
+                gamePause();
+                Cursor.visible = true;
+            }
+        }
+    }
+
+    public void gameResume()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void gamePause()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
